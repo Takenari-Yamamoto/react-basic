1c84e2dada9363cd8254b81e9b309dda88dab8ad
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-04-29 15:56:11 +0900

    api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { Article, TextInput, Counter, ToggleButton } from './components';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [id, setId] = useState('Takenari-Yamamoto');
+  const ids = ['aws', 'google', 'facebook', 'Takenari-Yamamoto'];
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)];
+    setId(_id);
+  };
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setName(data.name);
+      })
+      .catch((e) => console.log('ERROR'));
+  }, [id]);
+
   return (
     <div>
-      {/* <TextInput />
-      <Article title={'TITLEです'} content={'コンテンツです'} />
-      <Counter /> */}
-      <ToggleButton />
+      <p>GitHub </p>
+      <p>NAME: {name}</p>
+      <button onClick={getRandomId}>ID を変更</button>
     </div>
   );
 }
