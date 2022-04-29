3b1d1c75de51201298f698feeba579ca40ce99bf
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-04-29 17:07:33 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  const [name, setName] = useState('');
-  const [id, setId] = useState('Takenari-Yamamoto');
-  const ids = ['aws', 'google', 'facebook', 'Takenari-Yamamoto'];
-  const getRandomId = () => {
-    const _id = ids[Math.floor(Math.random() * ids.length)];
-    setId(_id);
-  };
-
-  useEffect(() => {
-    (async () => {
-      const res = await fetch(`https://api.github.com/users/${id}`);
-      const json = await res.json();
-      console.log(json);
-      setName(json.name);
-    })();
-  }, [id]);
-
-  return (
-    <div>
-      <p>GitHub </p>
-      <p>NAME: {name}</p>
-      <button onClick={getRandomId}>ID を変更</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
